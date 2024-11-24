f45e702b7280fcfebe938e5b2e98b93d84c79923
limard687 <119104115+limard687@users.noreply.github.com>
2024-11-24 14:43:33 +0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Employee
+    {
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeePost { get; set; }
+    }
+
+    public class clsEmployeeManager
+    {
+        public List<Employee> lstemployees = new List<Employee>();
+
+        public clsEmployeeManager()
+        {
+            lstemployees.Add(new Employee() { EmployeeId = "1", EmployeeName = "Матвей", EmployeePost = "Руководитель"});
+            lstemployees.Add(new Employee() { EmployeeId = "2", EmployeeName = "Степан", EmployeePost = "Начальник" });
+            lstemployees.Add(new Employee() { EmployeeId = "3", EmployeeName = "Михаил", EmployeePost = "Главный механик" });
+        }
+    }
+}
---
+++ EmployeeListPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static WpfApp1.EmployeeListPage;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeListPage.xaml
+    /// </summary>
+    public partial class EmployeeListPage : Page
+    {
+        public EmployeeListPage()
+        {
+            InitializeComponent();
+
+            clsEmployeeManager clsEmployeeManager = new clsEmployeeManager();
+
+            Employees_Collection.ItemsSource = clsEmployeeManager.lstemployees;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            EmployeeListPage_Frame.Content = (new EmployeeEditPage());
+        }
+    }
+}
